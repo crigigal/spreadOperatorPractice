72a6e31654756f22ee55dfedfad15093ff540eed
crigigal <50228889+crigigal@users.noreply.github.com>
2021-07-19 21:53:36 +0100

    Final Solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
 
 function App() {
+  const [task, setTask] = React.useState("");
+  const [list, setList] = React.useState([]);
+
+  function handleChange(event) {
+    const newValue = event.target.value;
+    setTask(newValue);
+  }
+
+  function addElement() {
+    setList((prevValue) => {
+      return [...prevValue, task];
+    });
+    setTask("");
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input onChange={handleChange} type="text" value={task} />
+        <button onClick={addElement}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {list.map((toDoItem) => (
+            <li>{toDoItem}</li>
+          ))}
         </ul>
       </div>
     </div>
